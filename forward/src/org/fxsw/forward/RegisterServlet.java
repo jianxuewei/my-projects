4cf336646a5bd2c3dd45ad76a0e5bcd9a2ec383a
jianxuewei <jianxuewei@126.com>
2015-02-09 17:08:31 +0800

    register.do

--- forward/src/org/fxsw/forward/RegisterServlet.java
+++ forward/src/org/fxsw/forward/RegisterServlet.java
@@ -1,6 +1,8 @@
 package org.fxsw.forward;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,14 +28,35 @@ public class RegisterServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=gbk");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print(" 用户名是"+request.getParameter("name")+"<br/>");
+		System.out.println(request.getParameter("name"));
+		out.print(" 密码是"+request.getParameter("password")+"<br/>");
+		System.out.println(request.getParameter("password"));
+		String sex=new String(request.getParameter("sex").getBytes("ISO-8859-1"),"GBK");
+		out.print(" 性别是"+ sex+"<br/>");
+		
+		System.out.println(request.getParameter("sex"));
+		out.print(" 年龄是"+Integer.parseInt(request.getParameter("age"))+"<br/>");
+		/*out.print(this.getClass());
+		out.println(", using the GET method");*/
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
